15181c04ccbc4f87197506d8581ca3d7bf3ed290
kakoyras <99966852+kakoyras@users.noreply.github.com>
2022-02-19 20:37:59 +0200

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,70 +1,73 @@
 from urllib.request import Request, urlopen
 import json
 
-req = Request('https://drand.cloudflare.com/public/latest', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0'})
-data = urlopen(req).read()
+k=0 #Βοηθητικοί μετρητές για παρακάτω
+m=0
+n=0
 
-opap = Request('https://api.opap.gr/draws/v3.0/1100/last-result-and-active', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0'})
-kino = urlopen(opap).read()
-kino = kino.decode()
-kino = json.loads(kino)
-kino = kino["last"]
-kino = kino["winningNumbers"]
-kino = kino["list"]
+req=Request("https://drand.cloudflare.com/public/latest", headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0"})
+data=urlopen(req).read()
+
+opap=Request("https://api.opap.gr/draws/v3.0/1100/last-result-and-active", headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20130401 Firefox/31.0"})
+kino=urlopen(opap).read()
+kino=kino.decode()
+kino=json.loads(kino)
+kino=kino["last"]
+kino=kino["winningNumbers"]
+kino=kino["list"]
 
 print(data)                     
-middata = str(data)
-finaldata = middata[33:97]          
-print ('Η σειρά του randomness είναι:',finaldata)
-pairs = []
-ints = [] 
-duos = []
+middata=str(data)
+finaldata=middata[33:97]          
+print ("Randomness: ",finaldata)
+pairs=[]
+ints=[] 
+duos=[]
 
-j = 0 
-print ('Τα ζεύγη που προκύπτουν είναι τα εξής:')
+print ("Τα ζεύγη που προκύπτουν: ")
 for i in range (32):  
-    a = (finaldata[j:j+2])
+    a=(finaldata[k:k+2])
     pairs.insert (i,a)
-    j = j + 2
+    k=k+2
 print (pairs)
-j = 0
-print ('Από την μετατροπή σε ακεραίους προκύπτουν τα εξής:')
+
+print ("Μετά την μετατροπή: ")
 for i in range (32):
-    a = (int(finaldata[j:j+2],16))   
+    a=(int(finaldata[m:m+2],16))   
     ints.insert (i,a)
-    j = j + 2
+    m=m+2
 print(ints)
-j = 0       
-print ('Μετά την πράξη modulo 80 έχουμε τα εξής:')
-for i in range (32):  
-    a = ((int(finaldata[j:j+2],16))%80)
+      
+print ("Μετά την πράξη mod 80: ")
+for i in range(32):  
+    a=((int(finaldata[n:n+2],16))%80)
     duos.insert(i,a)
-    j = j + 2
+    n=n+2
 print(duos)
-                #ταξινόμηση λίστας
-
+      
 duos.sort() 
-print ("Η λίστα μετά από την αύξουσα ταξινόμηση της:")
+print ("Η λίστα ταξινομιμένη: ")
 print (duos)
-j = 31
 
-                #αφαίρεση ίδιων αριθμών
-
-for i in range (j):  
-    end = "True"
-    if i+1 >= j:
+j=31
+for i in range(31):  
+    end="True"
+    if i+1>=j:
         break 
-    while end == "True":
-        if duos[i] == duos[i+1]:
+    while end=="True":
+        if duos[i]==duos[i+1]:
             duos.pop(i+1)
-            j = j - 1
-        elif duos[i] != duos[i+1]:
-            end = "False"
-print ("Η λίστα μετά την αφαίρεση των ίδιων αριθμών:")
+            j=j-1
+        elif duos[i]!=duos[i+1]:
+            end="False"
+print ("Η λίστα χωρίς ίδιους αριθμούς: ")
 print (duos)
-print ("Οι τελευταίοι αριθμοί που κληρώθηκαν στο κίνο είναι οι εξής:")           
+
+print ("Οι τελευταίοι αριθμοί που κληρώθηκαν στο κίνο: ")           
 print (kino)
-for i in range (20):
-    for j in range (len(duos)):
-        if kino[i] == duos[j]:
-            print("Ο αριθμός",duos[j],"από τoυς τυχαίους αριθμούς κληρώθηκε στο κίνο")
+
+for i in range(20):
+    for f in range(len(duos)):
+        if kino[i]==duos[f]:
+            print("Ο αριθμός ", duos[f], " από τoυς τυχαίους αριθμούς κληρώθηκε στο κίνο")
+            
